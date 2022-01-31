ac4e1b06802c7c66b31ca89c63faa6c044eeccf9
Lorenz Mueller <lorenz.mueller@squer.at>
2022-01-31 17:47:08 +0100

    Change port

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,7 +1,7 @@
 
 describe('The page should load', ()=> {
     it('Finds the line "Notes" on the page', ()=> {
-        cy.visit('http://localhost:3000');
+        cy.visit('http://localhost:4200');
         cy.contains('Notes')
     })
 })
